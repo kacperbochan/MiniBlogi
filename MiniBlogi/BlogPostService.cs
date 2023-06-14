434f56f9385c7755e0189cd763543383475610d9
kacperbochan <kacperbochan@gmail.com>
2023-06-14 13:52:52 +0200

    Controller + Services Work in progress

---
+++ MiniBlogi/BlogPostService.cs
@@ -0,0 +1,46 @@
+﻿using MiniBlogi.Controllers.Interfaces;
+using MiniBlogi.Models;
+
+namespace MiniBlogi
+{
+    public class BlogPostService
+    {
+        private readonly IBlogPostRepository _blogPostRepository;
+
+        public BlogPostService(IBlogPostRepository blogPostRepository)
+        {
+            _blogPostRepository = blogPostRepository;
+        }
+
+        public IEnumerable<BlogPost> GetAllBlogPosts()
+        {
+            return _blogPostRepository.GetAll();
+        }
+
+        public BlogPost GetBlogPostById(int id)
+        {
+            return _blogPostRepository.GetById(id);
+        }
+
+        public void AddBlogPost(BlogPost blogPost)
+        {
+            // Here we could add some business logic before saving to the database.
+            _blogPostRepository.Add(blogPost);
+            _blogPostRepository.Save();
+        }
+
+        public void UpdateBlogPost(BlogPost blogPost)
+        {
+            // And also here, for example validating the changes.
+            _blogPostRepository.Update(blogPost);
+            _blogPostRepository.Save();
+        }
+
+        public void DeleteBlogPost(int id)
+        {
+            _blogPostRepository.Delete(id);
+            _blogPostRepository.Save();
+        }
+    }
+
+}
---
+++ MiniBlogi/Controllers/BlogPostController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniBlogi.Controllers.Interfaces;
+using MiniBlogi.Models;
+
+namespace MiniBlogi.Controllers
+{
+    public class BlogPostController : Controller
+    {
+        private readonly IBlogPostRepository _blogPostRepository;
+
+        public BlogPostController(IBlogPostRepository blogPostRepository)
+        {
+            _blogPostRepository = blogPostRepository;
+        }
+
+        // List all blog posts
+        public async Task<IActionResult> Index()
+        {
+            var blogPosts = await _blogPostRepository.GetAllAsync();
+            return View(blogPosts);
+        }
+
+        // Get blog post by id
+        public async Task<IActionResult> Details(int id)
+        {
+            var blogPost = await _blogPostRepository.GetByIdAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+            return View(blogPost);
+        }
+
+        // Create a new blog post
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                await _blogPostRepository.AddAsync(blogPost);
+                await _blogPostRepository.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blogPost);
+        }
+
+        // Edit a blog post
+        public async Task<IActionResult> Edit(int id)
+        {
+            var blogPost = await _blogPostRepository.GetByIdAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+            return View(blogPost);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, BlogPost blogPost)
+        {
+            if (id != blogPost.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _blogPostRepository.Update(blogPost);
+                await _blogPostRepository.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blogPost);
+        }
+
+        // Delete a blog post
+        public async Task<IActionResult> Delete(int id)
+        {
+            var blogPost = await _blogPostRepository.GetByIdAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+            return View(blogPost);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            _blogPostRepository.Delete(id);
+            await _blogPostRepository.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+
+}
